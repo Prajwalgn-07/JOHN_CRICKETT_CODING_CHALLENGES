db2fc81e4fb5ba72e63f1bdcb89cef292cb18ccb
Prajwal Gowda GN <prajwal.gowda2499@gmail.com>
2024-03-21 19:33:57 +0530

    Update combinations.go

--- challenge49/src/algorithm/combinations.go
+++ challenge49/src/algorithm/combinations.go
@@ -1,20 +1,37 @@
 package algorithm
 
-func GetAllBruteForceCombinationsForTheSpecifiedLength(length int) []string {
-	const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789@#!()"
-	var combinations []string
-	for i := 1; i <= length; i++ {
-		generateCombinations(alphabet, length, "", &combinations)
-	}
+import (
+	"math"
+	"sync"
+)
+
+func GetAllParallelCombinationsForTheSpecifiedLength(length int) []string {
+	const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	combinations := make([]string, 0, int(math.Pow(float64(len(alphabet)), float64(length))))
+
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+
+	generateParallelCombinations(&wg, &mu, alphabet, length, "", &combinations)
+
+	wg.Wait()
+
 	return combinations
 }
 
-func generateCombinations(alphabet string, length int, prefix string, combinations *[]string) {
+func generateParallelCombinations(wg *sync.WaitGroup, mu *sync.Mutex, alphabet string, length int, prefix string, combinations *[]string) {
 	if length == 0 {
+		mu.Lock()
 		*combinations = append(*combinations, prefix)
+		mu.Unlock()
 		return
 	}
+
 	for _, char := range alphabet {
-		generateCombinations(alphabet, length-1, prefix+string(char), combinations)
+		wg.Add(1)
+		go func(char rune) {
+			defer wg.Done()
+			generateParallelCombinations(wg, mu, alphabet, length-1, prefix+string(char), combinations)
+		}(char)
 	}
 }
